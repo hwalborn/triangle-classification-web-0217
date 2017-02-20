80128826e33ade65aa5bcb33bf10879fb73b1811
hwalborn <hwalborn@gmail.com>
2017-02-20 16:04:35 -0500

    angled

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    arr = [side_1, side_2, side_3]
+    if arr.uniq.count == 1 && !(arr.include?(0))
+      :equilateral
+    elsif arr.uniq.count == 2 && !(triangle_inequality(arr))
+      :isosceles
+    elsif triangle_inequality(arr) || arr.any?{|num| num <= 0}
+      raise TriangleError
+    else
+      :scalene
+    end
+  end
+
+  def triangle_inequality(arr)
+    arr.each do |number|
+      if arr.reduce(:+) - number <= number
+        return true
+      end
+    end
+    false
+  end
+
+end
+
+class TriangleError <StandardError
 end
